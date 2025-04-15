c3d299cc840f6dd3c02aaf4fe4213ceb5c80704f
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2025-04-15 16:47:15 +0300

    add transient scope

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -10,12 +10,13 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  Scope,
 } from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { CreateSongDTO } from './dto/create-song.dto';
 import { Connection } from 'src/common/constants/connection';
 
-@Controller('songs')
+@Controller({ path: 'songs', scope: Scope.REQUEST })
 export class SongsController {
   //! with private can use songsService in methods this class
   constructor(
